a74007a18558c213551b01baabb71bec1f1fe8b7
GFV450 <gfv450@gmail.com>
2017-02-15 09:20:30 -0800

    Added semi-functional views, configured mongoDB

--- app.js
+++ app.js
@@ -1,33 +1,79 @@
 var express = require('express')
+var handlebars = require('express-handlebars');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
 var app = express()
-var exphbs  = require('express-handlebars')
 
-app.use(express.static('public'))
+mongoose.Promise = global.Promise;
 
-//handlebars-express
-app.engine('handlebars', exphbs({defaultLayout: 'main'}))
-app.set('view engine', 'handlebars')
+//Middleware
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-    res.render('home')
-})
+mongoose.connect('mongodb://localhost/ExpressJS')
+var Post = require('./models/post.js')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
 
-var posts = [ {body: "One"}, {body: "Two"}, {body: "Three"} ]
+}));
 
-// POST method route
-app.get('/posts', function (req, res) {
-	res.send(posts)
+//Variables
+var posts = [
+]
+
+//Home
+ app.get('/', function (req, res) {
+     Post.find().exec(function (err, posts) {
+         res.render('home.handlebars', {posts:posts});
+     })
  })
 
-var Post = require('./models/post.js')
+//Index
+app.get('/posts',function(req, res) {
+    Post.find().exec(function (err, posts) {
+        res.render('posts.handlebars', {posts:posts});
+    })
+});
 
-app.post('/posts', function (req, res) {
-    var post = req.body
-    post.create(post, function (err, post) {
-        res.status(200).json(post)
+//Show
+app.get('/posts/:id', function(req, res) {
+    var post = posts[req.params.id]
+    Post.findById(req.params.id).exec(function (err, post) {
+        res.render('show-post', {post:post})
     })
 })
 
+//Create
+app.post('/posts', function(req, res) {
+    var post = req.body;
+    Post.create(post, function (err, post) {
+        res.status(200).json(post);
+    })
+})
+
+//Delete
+app.delete('/posts/:id', function(req, res) {
+
+    Post.findById(req.params.id).exec(function (err, post) {
+        post.remove()
+        res.status(200).json({})
+    })
+})
+
+//Update
+app.get('/login', function (req, res) {
+    res.render('login.handlebars')
+});
+
+app.get('/signup', function (req, res) {
+    res.render('signup.handlebars')
+});
+
+
+//Port
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Listening on port 3000!')
 })
---
+++ controllers/posts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+    $( "#new-post" ).on( "submit", function( event ) {
+        event.preventDefault();
+
+        var post = $(this).serialize();
+
+        $.post('/posts', post, function (data) {
+            console.log(data)
+            $('#post-list').append("<li class= 'list-group-item'>" +
+            "<a href='/posts/" + data._id + "'>" + data.body + "</a>" +
+            "<div class='remove-post' data-id='" + data._id +
+             "'><i class='ion-trash-a'></i></div>" + "</li>");
+            $('#new-post')[0].reset();
+        });
+    });
+
+    $('#post-list').on("click", ".remove-post",  function (e) {
+        e.preventDefault();
+        console.log("remove-post");
+        var post = $(this)
+        var postId = post.data('id');
+
+        $.ajax({
+            url: '/posts/' + postId,
+            type: 'DELETE',
+            success: function(data) {
+                post.parent().remove();
+            }
+        })
+    })
+});
